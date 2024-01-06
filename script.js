1917279b3466cdd4a0fcc65c5d0237571b677497
Shubham-Dutta <30752162+Shubham-Dutta@users.noreply.github.com>
2024-01-06 13:04:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.addEventListener("load", () =>{
     document.querySelector(".page-loader").classList.add("fade-out");
     setTimeout(() =>{
         document.querySelector(".page-loader").style.display = "none";
-    },600);
+    },300);
 });
 
 /* ----------------Toggle Navbar---------------- */ 
